7bdf7291ef18b6011793f35f1c0bfac23ff9fc25
toleap <toleap0203@gmail.com>
2023-05-31 18:31:23 +0800

    1

--- ev_api_server/db/index.js
+++ ev_api_server/db/index.js
@@ -6,5 +6,12 @@ const db = mysql.createPool({
   password: "030111",
   database: "my_db_01",
 });
-
+/* // 测试mysql模块是否能正常工作
+db.query("select 1", (err, results) => {
+  // 模块工作期间报错了
+  if (err) return console.log(err.message);
+  // 成功了
+  console.log(results);
+});
+ */
 module.exports = db;
